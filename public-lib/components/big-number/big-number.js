b7ecd0dc08d0de3061dce56cdfc0b29ab269e15d
Robby Schlesinger <robbyschlesinger@Robbys-MacBook-Pro.local>
2017-04-16 06:18:52 -0700

    big number

--- public-lib/components/big-number/big-number.js
+++ public-lib/components/big-number/big-number.js
@@ -1,32 +1,19 @@
 import classes from 'app-utils/classes';
+import styles from './big-number.scss';
 
 import React, { Component } from 'react';
 
 export default class Table extends Component {
+	static propTypes = {
+		category: React.PropTypes.string,
+		amount: React.PropTypes.number,
+	};
 	render() {
 		return (
 			
-            <div className="content">
-                <span className="counter" data-count="24">24</span>
-                <p>December 2017</p>
-                <div id="clockdiv">
-                    <div className="clock-item">
-                        <span className="days">5</span>
-                        <p className="smalltext">Days</p>
-                    </div>
-                    <div className="clock-item">
-                        <span className="hours">4</span>
-                        <p className="smalltext">Hour</p>
-                    </div>
-                    <div className="clock-item">
-                        <span className="minutes">3</span>
-                        <p className="smalltext">Minu</p>
-                    </div>
-                    <div className="clock-item">
-                        <span className="seconds">2</span>
-                        <p className="smalltext">Seco</p>
-                    </div>
-                </div>
+            <div className={styles.content}>
+                <span className={styles.counter} data-count="24">{ this.props.amount }</span>
+                <p>{ this.props.category }</p>
             </div>
 		);
 	}
--- public-src/pages/landing/landing.js
+++ public-src/pages/landing/landing.js
@@ -13,16 +13,19 @@ export default class Landing extends Component {
 								<div className="row">
 									<div className="col-md-6 col-sm-12 col-xs-12">
 										<div className="seven-content-left">
-											<div className="content">
-												<span>Meet</span> <span>Up</span>
-												<h2>BUSINESS CONFERENCE 2017</h2>
-											</div>
+											<BigNumber
+												amount={10}
+												category='accomplishments'
+											/>
 										</div>
 									</div>
 									<div className="col-md-6 col-sm-12 hidden-xs">
 										<div className="seven-content-right-bg">
 											<div className="seven-content-right">
-												<BigNumber/>
+												<BigNumber
+													amount={270}
+													category='points'
+												/>
 											</div>
 										</div>
 									</div>
