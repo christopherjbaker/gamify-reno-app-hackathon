e01947910e7e60ad2aa67cf3dc43813989ed7f3e
Robby Schlesinger <robbyschlesinger@Robbys-MacBook-Pro.local>
2017-04-16 06:58:01 -0700

    landing

--- public-lib/components/big-number/big-number.js
+++ public-lib/components/big-number/big-number.js
@@ -7,14 +7,14 @@ export default class Table extends Component {
 	static propTypes = {
 		category: React.PropTypes.string,
 		amount: React.PropTypes.number,
+		className: React.PropTypes.string,
 	};
 	render() {
 		return (
-			
-            <div className={styles.content}>
-                <span className={styles.counter} data-count="24">{ this.props.amount }</span>
-                <p>{ this.props.category }</p>
-            </div>
+		<div className={classes(styles.content, styles.main)}>
+			<span className={styles.counter} data-count="24">{ this.props.amount }</span>
+			<p>{ this.props.category }</p>
+		</div>
 		);
 	}
 }
