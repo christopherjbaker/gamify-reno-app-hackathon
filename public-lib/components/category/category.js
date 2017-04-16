dbe222ddb3831470a5d85f7faefbffb1665a1072
Robby Schlesinger <robbyschlesinger@Robbys-MacBook-Pro.local>
2017-04-16 02:58:36 -0700

    category

--- public-lib/components/category/category.js
+++ public-lib/components/category/category.js
@@ -1,3 +1,4 @@
+import styles from './category.scss';
 import classes from 'app-utils/classes';
 
 import React, { Component } from 'react';
@@ -6,31 +7,29 @@ export default class Category extends Component {
 	render() {
 		return (
 			<div>
-				<div className="col-md-12 col-sm-12 col-xs-12">
-					<div className="pricing-item fourth">
-						<div className="pricing-header">
-							<h3 className="name">PREMIUM</h3>
-							<h2 className="amount"><sup>$</sup>500</h2>
-							<a href="#">ticket now</a>
-						</div>{'{'}/* pricing-header */{'}'}
-						
-						<div className="pricing-options">
-							<div>
-								<p>Conference Tickets<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
-								<p>Free Lunch And Coffee<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
-								<p>Easy Access<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
-								<p>Certificate<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
-								<p>Printed Metarials<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
-								<p>More Options<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
-							</i></i></i></i></i></i></div><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true">
-						</i></i></i></div><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true">{'{'}/* pricing-options */{'}'}
-						
-						<div className="pricing-button">
-							<a href="#">ticket now</a>
-						</div>
-					</i></i></i></div><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true">{'{'}/* pricing item */{'}'}
-				</i></i></i></div><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true">
-			</i></i></i></div>
+				<div className={classes(styles.pricingItem, styles.fourth)}>
+					<div className={styles.pricingHeader}>
+						<h3 className="name">PREMIUM</h3>
+						<h2 className="amount"><sup>$</sup>500</h2>
+						<a href="#">ticket now</a>
+					</div>
+					
+					<div className={ styles.pricingOptions }>
+						<div>
+							<p>Conference Tickets<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
+							<p>Free Lunch And Coffee<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
+							<p>Easy Access<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
+							<p>Certificate<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
+							<p>Printed Metarials<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
+							<p>More Options<span><i className="fa fa-check" aria-hidden="true" /></span></p><i className="fa fa-check" aria-hidden="true">
+						</i></i></i></i></i></i></div><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true">
+					</i></i></i></div><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true"><i className="fa fa-check" aria-hidden="true">{'{'}/* pricingOptions */{'}'}
+					
+					<div className={styles.pricingButton}>
+						<a href="#">ticket now</a>
+					</div>
+				</i></i></i></div>
+			</div>
 		);
 	}
 }
