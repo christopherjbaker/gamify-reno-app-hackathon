2f485738b2c3c50b290edc414620a6cc87db4fec
Robby Schlesinger <robbyschlesinger@Robbys-MacBook-Pro.local>
2017-04-16 03:40:41 -0700

    category

--- public-lib/components/category/category.js
+++ public-lib/components/category/category.js
@@ -10,14 +10,38 @@ export default class Category extends Component {
 	};
 	render() {
 		return (
-			<div className={ classes(styles.main, 'container-fluid') }>
-				<div className={ classes(styles.title, 'col-md-6')}>
-					{ this.props.title }
+			<section className={classes(styles.main, 'pricing pricing-three')}>
+				<div>
+					<div className="container">
+						<div className="pricing-items">
+							<div className="row">
+								<div className="col-md-12 col-sm-12 col-xs-12">
+									<div className="pricing-item fourth">
+										<div className="pricing-header">
+											<h3 className="name">PREMIUM</h3>
+											<h2 className="amount">{ this.props.title }</h2>
+											<a href="#">ticket now</a>
+										</div>
+										<div className="pricing-options">
+											<div>
+												<p>Conference Tickets<span><i className="fa fa-check" aria-hidden="true" /></span></p>
+												<p>Free Lunch And Coffee<span><i className="fa fa-check" aria-hidden="true" /></span></p>
+												<p>Easy Access<span><i className="fa fa-check" aria-hidden="true" /></span></p>
+												<p>Certificate<span><i className="fa fa-check" aria-hidden="true" /></span></p>
+												<p>Printed Metarials<span><i className="fa fa-check" aria-hidden="true" /></span></p>
+												<p>More Options<span><i className="fa fa-check" aria-hidden="true" /></span></p>
+											</div>
+										</div>
+										<div className="pricing-button">
+											<a href="#">ticket now</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div className={ classes(styles.stats, 'col-md-6')}>
-					Stats
-				</div>
-			</div>
+			</section>
 		);
 	}
 }
