af5735bf6d5e4430c3f117945803e482d99cdcde
Christopher Baker <christopher@hmudesign.com>
2017-04-16 17:00:45 -0700

    landing tweaks

--- public-src/pages/landing/landing.js
+++ public-src/pages/landing/landing.js
@@ -4,7 +4,6 @@ import API from 'app-api/objective';
 import React, { Component } from 'react';
 import Table from 'app-components/table/table';
 import Page from 'app-components/page/page';
-import BigNumber from 'app-components/big-number/big-number';
 
 export default class Landing extends Component {
 	constructor() {
@@ -27,70 +26,39 @@ export default class Landing extends Component {
 			<Page>
 				<section className="banner-seven">
 					<div className="banner-overlay">
-						<div className="container" style={{ paddingTop: '15rem' }}>
-							<div>
-								<div className="row">
-									<div className="col-md-6 col-sm-12 col-xs-12">
-										<BigNumber
-											amount={10}
-											category='accomplishments'
+						<div className="container" style={{ padding: '14rem 0 8rem' }}>
+							<div className="row">
+								<div className="col-md-12 col-sm-12 col-xs-12">
+									{ this.state.nearby && this.state.nearby.length > 0 ? (
+										<Table
+											className={ styles.nearby }
+											title="Nearby Tasks"
+											headers={[
+												{ label: 'Title', key: 'title' },
+												{ label: 'Points', key: 'points' },
+											]}
+											data={ this.state.nearby }
+											route={ ({ id }) => `/objective/${id}` }
 										/>
-									</div>
-									<div className="col-md-6 col-sm-12">
-										<BigNumber
-											className={ styles.numRight }
-											amount={270}
-											category='points'
-										/>
-									</div>
-								</div>
-								<div className="row">
-									<div className="col-md-12 col-sm-12 col-xs-12">
-										<section className='pricing pricing-three'>
-											<div>
-												<div className="container">
-													<div className="pricing-items">
-														<div className="row">
-															<div className="col-md-12 col-sm-12 col-xs-12">
-																<div className="pricing-item">
-																	<div className="pricing-header">
-																		<h2 className='amount' style={{ fontSize: '4rem' }}>Task of the Day</h2>
-																	</div>
-																	<div className='pricing-options' style={{ transform: 'translateY(100%)' }}>
-																		<div>
-																			<p>Available Points<span style={{ backgroundColor: 'white', color: 'black', fontSize: '2rem', marginRight: '10rem', textAlign: 'left' }}>150</span></p>
-																		</div>
-																	</div>
-																	<div className="pricing-button">
-																		<a href="#">ticket now</a>
-																	</div>
-																</div>
-															</div>
-														</div>
-													</div>
-												</div>
-											</div>
-										</section>
-									</div>
-								</div>
-								<div className="row" style={{ paddingTop: '5rem' }}>
-									<div className="col-md-12 col-sm-12 col-xs-12">
-										{ this.state.nearby && this.state.nearby.length > 0 ? (
-											<Table
-												className={ styles.nearby }
-												title="Nearby Tasks"
-												headers={[
-													{ label: 'Title', key: 'title' },
-													{ label: 'Points', key: 'points' },
-												]}
-												data={ this.state.nearby }
-												route={ ({ id }) => `/objective/${id}` }
-											/>
-										) : null }
-									</div>
+									) : null }
 								</div>
 							</div>
 						</div>
+					</div>
+				</section>
+				<section id="about" className="about about-six padding-120">
+					<div className="container">
+						<div className="section-header text-center">
+							<h2>About GamifyReno</h2>
+							<p><em>Reno as an Interactive, Social Scavenger Hunt</em></p>
+						</div>
+						<p>
+							For any who played Pokemon Go last summer, this idea should feel kind of familiar.
+							The idea is to take a city, in this case Reno, and turn it into an interactive,
+							social scavenger hunt. Through a web-based app, the user is able to seek and
+							complete objectives scattered through the city, encouraging the user to explore
+							and interact with other players as well as their neighbors and nearby friends.
+						</p>
 					</div>
 				</section>
 			</Page>
